0be9f6c27f60421574cac4ba70939f3bd5e636fe
MarkVL <v.o.markin221@gmail.com>
2026-08-13 08:18:55 +0300

    update example

--- examples/003-test-param-example/test-param-example.cpp
+++ examples/003-test-param-example/test-param-example.cpp
@@ -5,24 +5,22 @@
 FCF_TEST_BEFORE_DEFINE("*", "*", "*", fcf::NTest::FL_GLOBAL){
   fcf::NTest::TestPath path = {"Library", "Math", "sum"};
   fcf::NTest::storage().appendParamValue(path, std::string("one")
-                                             , std::string("two")
-                                             , std::string("three"));
+                                                , std::string("two")
+                                                , std::string("three"));
 }
 
 FCF_TEST_DEFINE("Library", "Math", "sum"){
   // Get the test parameter value
-  fcf::NTest::log() << "Current test parameter: " << *fcf::NTest::state().param().cast<std::string>()  << std::endl;
+  fcf::NTest::log() << "Current test parameter: " << *fcf::NTest::state().param().cast<std::string>() << std::endl;
 }
 
 int main(int a_argc, char* a_argv[]) {
   // Add test parameters passed via the command line
-  int argIndex = 1;
-  while(argIndex < a_argc) {
+  for(int argIndex = 1; argIndex < a_argc; ++argIndex) {
     if (std::strcmp(a_argv[argIndex], "--param") == 0 && (argIndex + 1)  < a_argc) {
       fcf::NTest::storage().appendParamValue("Library", "Math", "sum", std::string(a_argv[argIndex+1]));
       ++argIndex;
     }
-    ++argIndex;
   }
   // Start testing execution
   bool error = false;
